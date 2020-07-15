4e18e877301f106bf6eb5715a3a102d3837a2910
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-07-15 05:13:22 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
-const communicationSocial = document.getElementById("communication");
+
+function emailView() {
+    alert("\r\nBusiness Inquiry: ericznguyen@gmail.com \r\n \r\nOther Inquiry: enguyen6@uci.edu");
+}
+
+
+
+
+/*const communicationSocial = document.getElementById("communication");
 
 communicationSocial.addEventListener('click', ()=>{
 	alert ("\r\nBusiness Inquiry: ericznguyen@gmail.com \r\n \r\nOther Inquiry: enguyen6@uci.edu");
-})
+})*/
