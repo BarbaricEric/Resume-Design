43d7b51192edec4491851f276dab63efeba14d87
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-07-22 05:38:59 -0700

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,1 +1,8 @@
 const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu-navigation');
+
+
+
+hamburger.addEventListener('click', ()=>{
+	menu.classList.toggle("spread")
+})
