43434bc2b691b4f36c7bdf8129604015c29e9f3b
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-07-27 00:27:11 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@ const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
 const communicationSocial = document.getElementById("communication");
 
-function emailView() {
-    alert("\r\nBusiness Inquiry: ericznguyen@gmail.com \r\n \r\nOther Inquiry: enguyen6@uci.edu");
-}
-
+//Listen for contact link
 communicationSocial.addEventListener('click', ()=>{
-	alert ("\r\nBusiness Inquiry: ericznguyen@gmail.com \r\n \r\nOther Inquiry: enguyen6@uci.edu");
+	alert ("\r\n \r\nBusiness Inquiry: ericznguyen@gmail.com \r\n \r\nOther Inquiry: enguyen6@uci.edu");
 })
+
+// Listen for tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
+
+/*function emailView() {
+    alert("\r\n \r\nBusiness Inquiry: ericznguyen@gmail.com \r\n \r\nOther Inquiry: enguyen6@uci.edu");
+}*/
