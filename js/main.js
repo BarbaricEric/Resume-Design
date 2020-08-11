a1c0d40a36741ff61db969581acfbc816be54680
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-08-11 04:22:31 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,19 @@ const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
 const communicationSocial = document.getElementById("communication");
 
+//View tab content
+function selectItem() {
+  
+}
+
+//Warning Message: Supported Browser
+function browseView() {
+    document.getElementById("warningmessage").innerHTML = "For best experience, view on desktop.";
+    document.getElementById("supportmessage").innerHTML = "Website may behave differently depending on each user's setting and device";	
+}
+
+broseView();
+
 //Listen for contact link
 communicationSocial.addEventListener('click', ()=>{
 	alert ("\r\n \r\nBusiness Inquiry: ericznguyen@gmail.com \r\n \r\nOther Inquiry: enguyen6@uci.edu");
@@ -9,7 +22,3 @@ communicationSocial.addEventListener('click', ()=>{
 
 // Listen for tab click
 tabItems.forEach(item => item.addEventListener('click', selectItem));
-
-/*function emailView() {
-    alert("\r\n \r\nBusiness Inquiry: ericznguyen@gmail.com \r\n \r\nOther Inquiry: enguyen6@uci.edu");
-}*/
