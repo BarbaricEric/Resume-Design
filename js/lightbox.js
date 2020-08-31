99ade1d91643c7866db939ebe806d1f7fdb14adc
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-08-31 04:20:45 -0700

    Update lightbox.js

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,4 +1,5 @@
-const images = 
-const imagesLight = 
-const contentLight = 
-const hamburger1 =
+//DOM Elements
+const images = document.querySelector('.img-gallery');
+const imagesLight = document.querySelector('.images-light');
+const contentLight = document.querySelector('.open-images');
+const hamburger1 = document.querySelector('.hamburger');
