573e0312829de5736eb0d7337d539e1432ce67a1
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-10-05 01:54:25 -0700

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,18 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu-navigation');
+const hamburgertext = document.querySelector('.hamburger-text');
 
 hamburger.addEventListener('click', ()=>{
 	menu.classList.toggle("spread")
 })
 
+hamburgertext.addEventListener('click', ()=>{
+	menu.classList.toggle("spread")
+})
+
 window.addEventListener('click', e=>{
 	if(menu.classList.contains('spread')
-	 && e.target != menu && e.target != hamburger ){
+	 && e.target != menu && e.target != hamburger && e.target != hamburgertext ){
 	 
          menu.classList.toggle("spread")
 	
