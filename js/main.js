2ec0cb8cf684b44bc5d24f62a2139f081f26b9d2
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-10-09 05:27:55 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const tabItems = document.querySelectorAll('.tab-item');
 const tabContentItems = document.querySelectorAll('.tab-content-item');
 const communicationSocial = document.getElementById("communication");
 const BROSWERSUPPORT = document.querySelector('.banner-support');
+const mainsite = document.querySelector('.banner-logo');
 
 //View tab content
 function selectItem() {
@@ -14,8 +15,8 @@ function removeShow() {
 
 //Warning Message: Supported Browser
 async function browseView() {
-    document.getElementById("warningmessage").textContent = "For best interactive experience, view on desktop.";
-    document.getElementById("supportmessage").textContent = "Website may behave differently depending on each user's setting and device";
+    //document.getElementById("warningmessage").textContent = "For best interactive experience, view on desktop.";
+    document.getElementById("supportmessage").textContent = "Website may behave differently depending on each user's device and device setting";
     BROSWERSUPPORT.style.display = 'none';
 }
 
@@ -26,6 +27,12 @@ setTimeout(() => {browseView();}, 5.0*1000);
 communicationSocial.addEventListener('click', ()=>{
 	//alert ("\r\n \r\nBusiness Inquiry: ericznguyen@gmail.com \r\n \r\nOther Inquiry: enguyen6@uci.edu");
 	  alert ("\r\nCommunication Inquiry: ericznguyen@gmail.com");
+})
+
+//Listen for contact link
+mainsite.addEventListener('click', ()=>{
+	const url = 'https://www.endesign.tk';
+        const win = window.open(url, '_blank');
 })
 
 // Listen for tab click
