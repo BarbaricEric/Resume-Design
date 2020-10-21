ec2fb8400cebbab38652dba3db3a0f67975fab07
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-10-21 06:29:57 -0700

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -4,6 +4,8 @@ const hamburgertext = document.querySelector('.hamburger-text');
 const exitmenu = document.querySelector('#logout');
 const accordionitem = document.querySelector('.accordion-container');
 const navbarco = document.querySelector('.social-media-list-alt');
+const closeone = document.querySelector('.menu-close');
+const closetwo = document.querySelector('.menu-close-text');
 
 /*hamburger.addEventListener('click', ()=>{
 	menu.classList.toggle("spread")
@@ -29,9 +31,20 @@ window.addEventListener('click', e=>{
 }) */
 
 window.addEventListener('click', e=>{
-	if(e.target == hamburger || e.target == hamburgertext ){
+	const result = hamburger.classList
+	const answer = hamburgertext.classList
+	const repruesta = closeone.classList
+	const resultone = closetwo.classList
+	
+	if(e.target == result || e.target == answer || e.target == repruesta || e.target == resultone ){
 		
          menu.classList.toggle("spread")
 	
 	}
+	
+	/*if(e.target == hamburger || e.target == hamburgertext ){
+		
+         menu.classList.toggle("spread")
+	
+	}*/
 })
