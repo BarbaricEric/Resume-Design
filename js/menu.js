312de4ee6d54ffba5866f61dd276f4cfc7f17d72
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-10-22 01:33:13 -0700

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -7,7 +7,7 @@ const navbarco = document.querySelector('.social-media-list-alt');
 const closeone = document.querySelector('.menu-close');
 const closetwo = document.querySelector('.menu-close-text');
 
-/*hamburger.addEventListener('click', ()=>{
+hamburger.addEventListener('click', ()=>{
 	menu.classList.toggle("spread")
 })
 
@@ -20,7 +20,7 @@ exitmenu.addEventListener('click', ()=>{
 })
 
 
-window.addEventListener('click', e=>{
+/*window.addEventListener('click', e=>{
 	if(menu.classList.contains('spread')
 	 && e.target != menu && e.target != hamburger && e.target != hamburgertext ){
 	 //&& (e.target = hamburger || e.target = hamburgertext ){
@@ -31,20 +31,10 @@ window.addEventListener('click', e=>{
 }) */
 
 window.addEventListener('click', e=>{
-	const result = hamburger.classList
-	const answer = hamburgertext.classList
-	const repruesta = closeone.classList
-	const resultone = closetwo.classList
 	
-	if(e.target == result || e.target == answer || e.target == repruesta || e.target == resultone ){
+	if(e.target == hamburger || e.target == hamburgertext || e.target == closeone || e.target == closetwo ){
 		
          menu.classList.toggle("spread")
 	
 	}
-	
-	/*if(e.target == hamburger || e.target == hamburgertext ){
-		
-         menu.classList.toggle("spread")
-	
-	}*/
 })
