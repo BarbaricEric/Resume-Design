5053453d04b47281ac54a98835d1105ff82733a2
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-11-30 04:50:39 -0800

    Update projectdoc.js

--- js/projectdoc.js
+++ js/projectdoc.js
@@ -1,5 +1,20 @@
-/* DOM Elements */
+/* GSAP Interface */
 gsap.from(".left-container", 2, {
  width: "0",
+ ease: Expo.easeInOut
+});
+
+gsap.from(".logo", 2, {
+ delay: 1.5,
+ y: 20,
+ opacity: 0,
+ ease: Expo.easeInOut
+});
+
+gsap.from(".info", 2, {
+ delay: 1.5,
+ y: 50,
+ opacity: 0,
+ scale: 2.5,
  ease: Expo.easeInOut
 });
