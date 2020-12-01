61487d4ee6016c52168a4d312b70e5cbaf5d74a8
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-12-01 05:30:00 -0800

    Update lightbox.js

--- js/lightbox.js
+++ js/lightbox.js
@@ -4,7 +4,6 @@ const imagesLight = document.querySelector('.images-light');
 const contentLight = document.querySelector('.open-images');
 const hamburger1 = document.querySelector('.hamburger');
 
-/*
 images.forEach(images =>{
 	images.addEventListener('click', ()=>{
 	 appendImages(images.getAttribute('src'))
@@ -19,11 +18,9 @@ contentLight.addEventListener('click', (e)=>{
 	}	
 })
 
-
 const appendImages = (images)=>{
 	imagesLight.src = images
 	contentLight.classList.toggle('show')
 	imagesLight.classList.toggle('showImage')
 	hamburger1.style.opacity = '0'	
 } 
-*/
