e15b09dcd7193285366fd4948249811a36c0c276
BarbaricEric <48937341+BarbaricEric@users.noreply.github.com>
2020-12-09 05:10:21 -0800

    Update lightbox.js

--- js/lightbox.js
+++ js/lightbox.js
@@ -3,6 +3,7 @@ const images = document.querySelectorAll('.img-gallery');
 const imagesLight = document.querySelector('.images-light');
 const contentLight = document.querySelector('.open-images');
 const hamburgerHold = document.querySelector('.hamburger-holder');
+const cardDetails = document.querySelectorAll('.card');
 
 images.forEach(images =>{
 	images.addEventListener('click', ()=>{
@@ -10,11 +11,12 @@ images.forEach(images =>{
 	})
 })
 
-window.addEventListener('click', (e)=>{
+imagesLight.addEventListener('click', (e)=>{
 	if(e.target !== contentLight){
 	 imagesLight.classList.toggle('show')
 	 contentLight.classList.toggle('showImage')
 	 hamburgerHold.style.opacity = '1'
+	 cardDetails.style.display = 'none'
 	}	
 })
 
@@ -22,5 +24,6 @@ const appendImages = (images)=>{
 	contentLight.src = images
 	imagesLight.classList.toggle('show')
 	contentLight.classList.toggle('showImage')
-	hamburgerHold.style.opacity = '0'	
+	hamburgerHold.style.opacity = '0'
+	cardDetails.style.display = "block'
 } 
